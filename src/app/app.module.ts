854a0075cef7e31f02cfc646330533212fe75db0
WeakedBUSH <AouimerRamziP@hotmail.com>
2022-07-04 22:50:28 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
 
 @NgModule({
+
   declarations: [
     AppComponent
   ],
   imports: [
+    MatIconModule,
+    MatSliderModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
